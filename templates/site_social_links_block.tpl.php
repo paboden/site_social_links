d9d5cea9ec631ff78fd4fe8c9ddb52d04a4d7e4c
Philip Boden <philip.boden@gmail.com>
2015-09-13 20:12:30 -0700

    Adding caching to the callback for the block content array

--- templates/site_social_links_block.tpl.php
+++ templates/site_social_links_block.tpl.php
@@ -1,12 +1,12 @@
 <?php
 
 /**
- * Displays the footer/breadcrumb social media icon block.
+ * Displays the social media icon block.
  */
 ?>
-<div class="social-links__container">
+<div class="social-links--container">
   <?php foreach ($social_icons as $icon) : ?>
-    <a href="http://<?php print $icon['url']; ?>" <?php if ($icon['target']) : print 'target="_blank"'; endif;?> class="social-icons__link">
+    <a href="http://<?php print $icon['url']; ?>" <?php if ($icon['target']) : print 'target="_blank"'; endif;?> class="social-icons--link">
       <span class="social-links fa <?php print $icon['class']; ?>">
         <?php if ($icon['display_name']) : print $icon['name']; endif;?>
       </span>
